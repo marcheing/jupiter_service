cce44bb679641235f8a2782643a9d65648af25a3
Heitor Reis <marcheing@gmail.com>
2016-08-02 08:52:38 -0300

    Add settings using settings logic

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module JupiterService
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Autoload settings file
+    config.autoload_paths += %W(#{config.root}/lib/settings)
   end
 end
