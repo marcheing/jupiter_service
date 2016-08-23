0bbe0915d02908583b5cda0c34cbead4c7a9b0a7
Heitor Reis <marcheing@gmail.com>
2016-08-23 07:36:02 -0300

    Finish subscription parser

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,9 +1,3 @@
 class Subscription
-  TYPES = [
-    'Obrigatória'.freeze,
-    'Optativa Eletiva'.freeze,
-    'Optativa Livre'.freeze
-  ].freeze
-
   attr_accessor :type, :assigned_to, :vacancy, :subscribed, :pending, :enrolled
 end
--- lib/jupiter/parser/subscription_parser.rb
+++ lib/jupiter/parser/subscription_parser.rb
@@ -1,6 +1,8 @@
 module Jupiter
   module Parser
     class SubscriptionParser < Base
+      attr_reader :subscriptions
+
       def initialize(doc)
         @doc = doc
         parse
@@ -9,6 +11,34 @@ module Jupiter
       private
 
       def parse
+        create_subscriptions
+      end
+
+      def create_subscriptions
+        @subscriptions = []
+        subscription_data = @doc.xpath('tr')[1..-1]
+        subscription_data.each do |row|
+          subscription = create_subscription(row)
+          subscription.type = @subscriptions.last.type if subscription.type.nil?
+          subscriptions << subscription
+        end
+      end
+
+      def create_subscription(row)
+        subscription = Subscription.new
+        fields = row.xpath('td')
+        first_field = fields[0].at_xpath('font/span')
+        if first_field.nil?
+          subscription.assigned_to = element_text_at_xpath(fields[1], 'font/span')
+          fields = fields[2..-1]
+        else
+          subscription.type = first_field.text.strip
+          fields = fields[1..-1]
+        end
+        [:vacancy, :subscribed, :pending, :enrolled].each do |subscription_field|
+          subscription.send("#{subscription_field}=", element_text_at_xpath(fields.shift, 'font/span').to_i)
+        end
+        subscription
       end
     end
   end
