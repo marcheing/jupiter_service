37f9cec67a5087542a396c5bed544959b09b190c
Heitor Reis <marcheing@gmail.com>
2016-10-11 11:20:57 -0300

    Add deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.6.1'
 
 set :application, 'jupiter_service'
-set :repo_url, 'git@github.com/marcheing/jupiter_service'
+set :repo_url, 'git@github.com:marcheing/jupiter_service'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/marcheing/jupiter_service'
+set :deploy_to, '/home/jupiter_service'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -24,7 +24,7 @@ set :deploy_to, '/home/marcheing/jupiter_service'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'bcc.ime.usp.br', %w(app web), primary: true
-set :deploy_to, '/home/marcheing/jupiter_service'
+server 'bcc.ime.usp.br', roles: [:web], primary: true, user: 'jupiter_service'
 
 # role-based syntax
 # ==================
