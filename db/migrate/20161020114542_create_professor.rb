123e50b7878c2fecf2f0d48b6386685fe87bf8f9
Heitor Reis <marcheing@gmail.com>
2016-10-20 10:56:36 -0200

    Add Professor table

---
+++ db/migrate/20161020114542_create_professor.rb
@@ -0,0 +1,9 @@
+class CreateProfessor < ActiveRecord::Migration[5.0]
+  def change
+    create_table :professors do |t|
+      t.integer :code
+      t.string :name
+      t.timestamps
+    end
+  end
+end
