c31066a2c1e0ac25f32777661480bb26cf35947b
Heitor Reis <marcheing@gmail.com>
2016-10-20 11:14:01 -0200

    Add created_at and updated_at filters to as_json on ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # Exclude internal id info from json output
+  # Exclude internal attributes info from json output
   def as_json(options = {})
-    options[:except] ||= [:id]
+    options[:except] ||= [:id, :created_at, :updated_at]
     super(options)
   end
 end
--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -4,12 +4,14 @@ describe ApplicationRecord do
   describe 'as_json' do
     context 'with a subclass that defines an id field' do
       let!(:the_subclass) { Professor }
-      let(:object) { build :professor }
+      let(:object) { create :professor }
       let(:object_attributes) { attributes_for(:professor).with_indifferent_access }
 
-      it 'adds the :id to the list of exceptions' do
+      it 'adds :id, :created_at and :updated_at to the list of exceptions' do
         object_as_json = object.as_json.with_indifferent_access
         expect(object_as_json.keys).to_not include('id')
+        expect(object_as_json.keys).to_not include('created_at')
+        expect(object_as_json.keys).to_not include('updated_at')
         object_attributes.each do |attribute, value|
           expect(object_as_json[attribute]).to eq value
         end
