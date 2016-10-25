62ecc3ea5bb544b891b17d2bc021a9ce538f57e0
Heitor Reis <marcheing@gmail.com>
2016-10-25 11:05:09 -0200

    Update application record and schedule to eliminate relation_ids
    Do not send ids as response, send the serialized objects instead.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,8 @@ class ApplicationRecord < ActiveRecord::Base
 
   # Exclude internal attributes info from json output
   def as_json(options = {})
-    options[:except] ||= [:id, :created_at, :updated_at]
+    options[:except] ||= []
+    options[:except] |= [:id, :created_at, :updated_at]
     super(options)
   end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,11 @@
 class Schedule < ApplicationRecord
   belongs_to :professor
+
+  def as_json(options = {})
+    options[:except] ||= []
+    options[:except] |= [:professor_id]
+    options[:methods] ||= []
+    options[:methods] |= [:professor]
+    super(options)
+  end
 end
